d861a857ad4c4e2a1464b475b69dfee09a448877
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2023-06-02 09:44:58 +0700

    add user collection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -11,6 +12,7 @@ class UserController extends Controller
     public function index(){
         $users = User::all();
         // return response()->json(["data" => $users]);
-        return UserResource::collection($users);
+        // return UserResource::collection($users);
+        return new UserCollection($users);
     }
 }
---
+++ app/Http/Resources/UserCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return [
+            // 'data' => $this->collection,
+            'data' => UserResource::collection($this->collection),
+            'meta' => [
+                'total_user' => $this->collection->count()
+            ],
+            'message' => "good",
+            'error' => false
+        ];
+    }
+}
